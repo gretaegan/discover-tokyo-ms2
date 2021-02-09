91ac026d85aae51bda758bf4933df0a45837b5db
gretaegan <greta.egan@gmail.com>
2021-02-09 17:07:10 +0000

    Add boilerplate for Seasons section

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+ // Google maps //        
+        
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById("map"), {
+                zoom: 3,
+                center: {
+                    lat: 35.8091702410353,
+                    lng: 139.7587132681937
+                }
+            });
+        }
