3374a055d6fa5282b1add8cd0c1eb5d174e9ba1e
gretaegan <greta.egan@gmail.com>
2021-02-10 14:58:51 +0000

    Create layout of When to Visit section using Bootstrap Grid and add an image card to each column

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,5 @@
- // Google maps //        
-        
-        function initMap() {
-            var map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 3,
-                center: {
-                    lat: 35.8091702410353,
-                    lng: 139.7587132681937
-                }
-            });
-        }
+$(document).ready(function(){
+  $("button").click(function(){
+    $("#spring").toggle();
+  });
+});
